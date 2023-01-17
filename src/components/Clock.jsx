ba86640267a1f37be7de4e1370bc0efbfdce6965
Paata Home <paata.tepnadze@gmail.com>
2023-01-17 10:29:39 +0100

    add react-clock / analog clock npm packages. react app clock samples

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-
+import AnalogClock from 'analog-clock-react';
 const Clock = () => {
+  let options = {
+    width: '300px',
+    border: '3vmin solid #000',
+    borderColor: '#2e2e2e',
+    baseColor: '#17a2b8',
+    centerColor: '#459cff',
+    centerBorderColor: '#ffffff',
+    handColors: {
+      second: '#d81c7a',
+      minute: '#ffffff',
+      hour: '#ffffff',
+    },
+  };
+
   return (
     <div>
-      <div className={'clock'}>
-        <h3>Clock</h3>
-        <div className={'analog-clock'}>
-          <div className={'dial seconds'} />
-          <div className={'dial minutes'} />
-          <div className={'dial hours'} />
-        </div>
-        <div className={'digital-clock'}></div>
-      </div>
+      <h2>React Clock - GeeksforGeeks</h2>
+      <AnalogClock {...options} />
     </div>
   );
 };
